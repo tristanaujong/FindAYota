882e8ef660fd40353e3ef9b954ab927683a4cd70
Rivan <rivan2k@tamu.edu>
2025-01-25 14:56:45 -0600

    reevaluated the most important financial values

--- finance.py
+++ finance.py
@@ -1,29 +1,19 @@
 class Finance:
-    def __init__(self, annual_income, desired_monthly, credit_score, 
-                 budget_range, down_payment=None, loan_term=None, monthly_debt=None, trade_in=None):
-        self.annual_income = annual_income
+    def __init__(self, desired_monthly, credit_score, down_payment=None, 
+                 loan_term=None, trade_in=None):
         self.desired_monthly = desired_monthly
         self.credit_score = credit_score
-        self.budget_range = budget_range
         self.down_payment = down_payment
         self.loan_term = loan_term
-        self.monthly_debt = monthly_debt
         self.trade_in = trade_in
-        self.DTI = None
 
-    def calc_DTI(self, monthly_debt):
-        if self.annual_income > 0:
-            monthly_income = self.annual_income/12
-            self.DTI = (monthly_debt / monthly_income) * 100
-            return self.DTI
-        elif self.annual_income == 0:
-            self.DTI = -1                                           # -1 value for zero income
-            return self.DTI
-        else: 
-            raise ValueError("Annual income must be $0 or more")
+        # bare minimums: down payment, desired monthly payment, credit score range, trade in value
+        # toyotas values: lease/finance/most affordable, monthly/cashdown/im not sure, trade in, 
+
         
-    
     # estimated affordable price
+
+    # estimate financial rate 
 
 
     # def repr(self)
