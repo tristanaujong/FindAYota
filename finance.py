57e2c45ab12ac378b3efa92438fbd58fe0b6dc8c
Rivan <rivan2k@tamu.edu>
2025-01-25 15:29:40 -0600

    pull request to standardize

--- finance.py
+++ finance.py
@@ -1,6 +1,5 @@
 class Finance:
-    def __init__(self, desired_monthly, credit_score, down_payment=None, 
-                 loan_term=None, trade_in=None):
+    def __init__(self, desired_monthly, credit_score, down_payment, loan_term, trade_in):
         self.desired_monthly = desired_monthly
         self.credit_score = credit_score
         self.down_payment = down_payment
@@ -10,7 +9,10 @@ class Finance:
         # bare minimums: down payment, desired monthly payment, credit score range, trade in value
         # toyotas values: lease/finance/most affordable, monthly/cashdown/im not sure, trade in, 
 
+    
         
+    # calculate APR
+
     # estimated affordable price
 
     # estimate financial rate 
