936dc597da0c82acbd0a9be48eb7b57a26693ce5
Larissa Kim <kimLarissa04@gmail.com>
2025-01-25 16:30:08 -0600

    hashmap done!

--- app.py
+++ app.py
@@ -7,20 +7,18 @@ from vehicle_info import Vehicle
 app = Flask(__name__)
 
 # make a function that instantiates a hashmap from json file
-vehicle_point_map = {}
 
 def create_map():
+    vehicle_point_map = {}
     with open("data/vehicles.json") as v:
         car_info = json.load(v)
     car_list = parse_vehicles(car_info) # list of cars
     # now make the hashset with model as key and points as value
     for car in car_list:
-        vehicle_point_map.update(car.get_model())
-    print(vehicle_point_map)
+        vehicle_point_map.update({car.get_model():0})
+    # print(vehicle_point_map)
     return vehicle_point_map
-
-    
-
+vehicle_point_map = create_map()
 
 
 @app.route("/")
@@ -41,7 +39,7 @@ def form():
         # compute vehicle points
             # call a function here that will return a hashmap with car to points pairs
         # Pass matched vehicles to the results page
-        return render_template("results.html", matches=matched_vehicles)    # matched vehicles is the hashmap returned by the "compute vehicle points" function
+        return render_template("results.html", matches=vehicle_point_map)    #  is the hashmap returned by the "compute vehicle points" function
     return render_template("form.html")
 
 # code in compute vehicle points function here
